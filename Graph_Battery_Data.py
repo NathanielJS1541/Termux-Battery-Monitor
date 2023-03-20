65cc49d980209da18595aa8e6406672da2921f0a
Nathaniel Struselis <Nathaniel.Struselis@mail.nidec.com>
2023-03-20 15:37:40 +0000

    Refactored script.

--- Graph_Battery_Data.py
+++ Graph_Battery_Data.py
@@ -6,7 +6,11 @@ import argparse  # argparse is used to parse command line arguments for the scri
 from datetime import datetime, timedelta  # datetime is used to manipulate times
 import json  # json library is used to parse the output from "termux-battery-status"
 from pathlib import Path  # pathlib allows a convenient way to handle paths
-from rich.console import Console # rich.console is used to colour output
+import subprocess  # subprocess allows running a command and capturing the output
+import time  # time has a simple .sleep() function for basic time delays
+import matplotlib.pyplot as plt  # matplotlib allows creation of graphs
+from rich.console import Console  # rich.console is used to colour output
+
 # rich.progress is used to create progress bars
 from rich.progress import (
     BarColumn,
@@ -16,10 +20,6 @@ from rich.progress import (
     TimeElapsedColumn,
     TimeRemainingColumn,
 )
-import subprocess  # subprocess allows running a command and capturing the output
-import time  # time has a simple .sleep() function for basic time delays
-from tqdm import tqdm  # tqdm is used to create a progress bar
-import matplotlib.pyplot as plt  # matplotlib allows creation of graphs
 
 # -------------------------------------------------------------------------------------
 
@@ -101,9 +101,10 @@ progress_bar = Progress(
 # Check if both a stop_percentage and stop_time have been specified. If they have, warn
 # the user that whichever is reached first takes president.
 if (args.stop_time is not None) and (args.stop_percentage != 100):
-    console.print("[WARN] Both a percentage and time have been specified. Whichever is"
+    console.print(
+        "[WARN] Both a percentage and time have been specified. Whichever is"
         + " reached first will cause the program to terminate.",
-        style="bold yellow"
+        style="bold yellow",
     )
 # -------------------------------------------------------------------------------------
 
@@ -129,7 +130,7 @@ current = []
 battery_bar = progress_bar.add_task("[blue]Battery", total=100)
 if args.stop_time is not None:
     time_bar = progress_bar.add_task("[yellow]Time", total=100)
-total_progress = progress_bar.add_task("[green]Total Progress", total = 100)
+total_progress = progress_bar.add_task("[green]Total Progress", total=100)
 
 # If verbose is specifed, print output that the loop is starting
 if args.verbose:
@@ -145,28 +146,36 @@ with progress_bar:
             output = proc.stdout.read().decode("utf-8")
             current_status = json.loads(output)
             current_time = datetime.now()
-            
+
             # If this is the first iteration, record the initial percentage
             if battery_initial == -1:
                 battery_initial = current_status["percentage"]
-            
+
             # Calculate the progress for the battery percentage
-            battery_progress = int(((current_status["percentage"] - battery_initial) / (args.stop_percentage - battery_initial)) * 100)
+            battery_progress = int(
+                (
+                    (current_status["percentage"] - battery_initial)
+                    / (args.stop_percentage - battery_initial)
+                )
+                * 100
+            )
 
             # Calculate the current progress for the stop time if it is specified
             if args.stop_time is not None:
                 time_progress = int(
                     ((current_time - start_time).total_seconds() / args.stop_time) * 100
                 )
-            
+
             # Use the bigger percentage as the overall progress, but cap it at 100
             current_progress = min(max(time_progress, battery_progress), 100)
 
             # Update the progress bars
-            progress_bar.update(battery_bar, advance=battery_progress-last_battery)
+            progress_bar.update(battery_bar, advance=battery_progress - last_battery)
             if args.stop_time is not None:
-                progress_bar.update(time_bar, advance=time_progress-last_time)
-            progress_bar.update(total_progress, advance=current_progress-last_progress)
+                progress_bar.update(time_bar, advance=time_progress - last_time)
+            progress_bar.update(
+                total_progress, advance=current_progress - last_progress
+            )
 
             # Record the relevant statistics
             timestamp.append((current_time - start_time).total_seconds())
@@ -188,7 +197,7 @@ with progress_bar:
             time.sleep(sleep_time)
 
 if args.verbose:
-    cprint("[VERBOSE] Monitoring loop finished.", "blue")
+    console.print("[VERBOSE] Starting monitoring loop...", style="blue")
 
 # After the loop ends, create plots with the given data
 if len(current) > 0:
@@ -214,5 +223,7 @@ if len(current) > 0:
 
     figure.tight_layout()
 
-    console.print(f"[INFO] Saving output to {args.output_file.resolve()}", style="green")
+    console.print(
+        f"[INFO] Saving output to {args.output_file.resolve()}", style="green"
+    )
     plt.savefig(args.output_file)
