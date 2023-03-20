1dc91efa72c7536929055701474747e26b228567
Nathaniel Struselis <Nathaniel.Struselis@mail.nidec.com>
2023-03-20 15:32:23 +0000

    Changed progress bars from tqdm to rich. Also fixed problems with remaining percentage not being calculated correctly.

--- Graph_Battery_Data.py
+++ Graph_Battery_Data.py
@@ -6,8 +6,17 @@ import argparse  # argparse is used to parse command line arguments for the scri
 from datetime import datetime, timedelta  # datetime is used to manipulate times
 import json  # json library is used to parse the output from "termux-battery-status"
 from pathlib import Path  # pathlib allows a convenient way to handle paths
+from rich.console import Console # rich.console is used to colour output
+# rich.progress is used to create progress bars
+from rich.progress import (
+    BarColumn,
+    Progress,
+    SpinnerColumn,
+    TaskProgressColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
 import subprocess  # subprocess allows running a command and capturing the output
-from termcolor import cprint  # termcolor allows easy printing of coloured text
 import time  # time has a simple .sleep() function for basic time delays
 from tqdm import tqdm  # tqdm is used to create a progress bar
 import matplotlib.pyplot as plt  # matplotlib allows creation of graphs
@@ -69,21 +78,43 @@ parser.add_argument(
 args = parser.parse_args()
 # -------------------------------------------------------------------------------------
 
+# ------------------------------------ Rich Setup -------------------------------------
+
+# Set up the rich console
+console = Console()
+
+# Set up a custom progress bar format
+progress_bar = Progress(
+    SpinnerColumn(),
+    "[progress.description]{task.description}",
+    BarColumn(),
+    TaskProgressColumn(),
+    "Elapsed:",
+    TimeElapsedColumn(),
+    "Remaining:",
+    TimeRemainingColumn(),
+)
+
+# -------------------------------------------------------------------------------------
 
 # --------------------------------- Input Validation ----------------------------------
 # Check if both a stop_percentage and stop_time have been specified. If they have, warn
 # the user that whichever is reached first takes president.
 if (args.stop_time is not None) and (args.stop_percentage != 100):
-    cprint(
-        "[WARN] Both a percentage and time have been specified. Whichever is reached "
-        + "first will cause the program to terminate.",
-        "yellow",
+    console.print("[WARN] Both a percentage and time have been specified. Whichever is"
+        + " reached first will cause the program to terminate.",
+        style="bold yellow"
     )
 # -------------------------------------------------------------------------------------
 
 # This will be used to track the monitoring progress
 last_progress = 0
 current_progress = 0
+battery_initial = -1
+battery_progress = 0
+last_battery = 0
+time_progress = 0
+last_time = 0
 
 # This records the time at which the script started for relative timings
 start_time = datetime.now()
@@ -94,11 +125,17 @@ percentage = []
 temperature = []
 current = []
 
+# Set up the progress bars
+battery_bar = progress_bar.add_task("[blue]Battery", total=100)
+if args.stop_time is not None:
+    time_bar = progress_bar.add_task("[yellow]Time", total=100)
+total_progress = progress_bar.add_task("[green]Total Progress", total = 100)
+
 # If verbose is specifed, print output that the loop is starting
 if args.verbose:
-    cprint("[VERBOSE] Starting monitoring loop...", "blue")
+    console.print("[VERBOSE] Starting monitoring loop...", style="blue")
 
-with tqdm(total=100, unit="%") as progress:
+with progress_bar:
     while current_progress < 100:
         # Get output from termux-battery-status command
         with subprocess.Popen(
@@ -108,19 +145,28 @@ with tqdm(total=100, unit="%") as progress:
             output = proc.stdout.read().decode("utf-8")
             current_status = json.loads(output)
             current_time = datetime.now()
+            
+            # If this is the first iteration, record the initial percentage
+            if battery_initial == -1:
+                battery_initial = current_status["percentage"]
+            
+            # Calculate the progress for the battery percentage
+            battery_progress = int(((current_status["percentage"] - battery_initial) / (args.stop_percentage - battery_initial)) * 100)
 
-            # Calculate the current progress from the specified stop conditions
-            current_progress = int(current_status["percentage"])
+            # Calculate the current progress for the stop time if it is specified
             if args.stop_time is not None:
                 time_progress = int(
                     ((current_time - start_time).total_seconds() / args.stop_time) * 100
                 )
-                current_progress = min(max(time_progress, current_progress), 100)
-            if args.verbose:
-                cprint(f"[VERBOSE] Current progress: {current_progress}", "blue")
+            
+            # Use the bigger percentage as the overall progress, but cap it at 100
+            current_progress = min(max(time_progress, battery_progress), 100)
 
-            # Update the progress bar
-            progress.update(current_progress - last_progress)
+            # Update the progress bars
+            progress_bar.update(battery_bar, advance=battery_progress-last_battery)
+            if args.stop_time is not None:
+                progress_bar.update(time_bar, advance=time_progress-last_time)
+            progress_bar.update(total_progress, advance=current_progress-last_progress)
 
             # Record the relevant statistics
             timestamp.append((current_time - start_time).total_seconds())
@@ -129,6 +175,9 @@ with tqdm(total=100, unit="%") as progress:
             # Reverse current sign as Termux-API convention is negative for charging
             current.append(-float(int(current_status["current"]) / 1000))
 
+            # Update historic variables
+            last_battery = battery_progress
+            last_time = time_progress
             last_progress = current_progress
 
             # Wait to take next reading
@@ -165,5 +214,5 @@ if len(current) > 0:
 
     figure.tight_layout()
 
-    cprint(f"[INFO] Saving output to {args.output_file.resolve()}", "green")
+    console.print(f"[INFO] Saving output to {args.output_file.resolve()}", style="green")
     plt.savefig(args.output_file)
